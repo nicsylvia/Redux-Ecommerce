d28ae41f4e471d814891686e68c3adc2a2c96ce6
nicsylvia <nicsylvia15f@gmail.com>
2023-02-15 15:40:52 -0800

    saved pages

---
+++ src/Components/AllRoutes/AllRoutes.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import {useRou}
+
+const AllRoutes = () => {
+  return (
+    <div>AllRoutes</div>
+  )
+}
+
+export default AllRoutes
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from "../Assets/logo.svg";
+import logo from "../Assets/isaac2.png";
 import { CiSearch } from "react-icons/ci"
+import { BsPerson } from "react-icons/bs"
+import { RiShoppingCartLine } from "react-icons/ri"
 
 const Header = () => {
   return (
@@ -18,9 +20,15 @@ const Header = () => {
             <Nav>Explore</Nav>
           </Navigation>
           <Profile>
+            <p>
             <CiSearch />
-            <CiSearch />
-            <CiSearch />
+            </p>
+            <p>
+            <BsPerson />
+            </p>
+            <p>
+            <RiShoppingCartLine />
+            </p>
           </Profile>
         </Wrapper>
       </Container>
@@ -33,7 +41,6 @@ export default Header;
 const Container = styled.div`
   width: 100%;
   height: 70px;
-  background-color: red;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -41,12 +48,30 @@ const Container = styled.div`
 const Wrapper = styled.div`
   width: 90%;
   height: 60px;
-  background-color: blue;
+  /* background-color: lightblue; */
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
-const Logo = styled.img``;
-const Navigation = styled.div``;
-const Nav = styled.div``;
-const Profile = styled.div``;
+const Logo = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+`;
+const Navigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Nav = styled.div`
+  margin-right: 30px;
+`;
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  p{
+    margin-right: 30px;
+    font-size: 25px;
+  }
+`;
