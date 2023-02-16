c4fb7910e41e7aa409cdcc13b8720a6ea8a3b4ef
nicsylvia <nicsylvia15f@gmail.com>
2023-02-16 09:32:14 -0800

    still on special kids

--- src/Components/SpecialKids/SpecialKids.tsx
+++ src/Components/SpecialKids/SpecialKids.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled from 'styled-components';
+import specialkid from "../Assets/specialkid.png"
+import wrapbg from "../Assets/wrap-bg.svg"
 
 const SpecialKids = () => {
   return (
     <div>
         <Container>
-            <Wrapper></Wrapper>
+            <Wrapper>
+                <Img>
+                    <img src={specialkid} alt="" />
+                </Img>
+                <Content></Content>
+            </Wrapper>
         </Container>
     </div>
   )
@@ -13,5 +20,41 @@ const SpecialKids = () => {
 
 export default SpecialKids;
 
-const Wrapper = styled.div``;
-const Container = styled.div``;
+const Img = styled.div`
+    width: 40%;
+    height: 100%;
+    background-color: yellow;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+const Content = styled.div`
+    width: 40%;
+    height: 80%;
+    background-color: hotpink;
+    margin-left: 30px;
+`;
+
+const Wrapper = styled.div`
+    width: 90%;
+    height: 600px;
+    border-radius: 15px;
+    background-color: #FEFCE8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(${wrapbg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+const Container = styled.div`
+    width: 100%;
+    padding: 20px 0px 20px 0px;
+    background-color: red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
