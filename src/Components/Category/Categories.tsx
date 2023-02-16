221b61577497f71760faebc7806c3ff13e9562ac
nicsylvia <nicsylvia15f@gmail.com>
2023-02-16 11:11:43 -0800

    working on category

--- src/Components/Category/Categories.tsx
+++ src/Components/Category/Categories.tsx
@@ -1,24 +1,92 @@
-import React from 'react'
+import React, {useState } from 'react'
 import styled from 'styled-components';
-import { RiWomenLine } from "react-icons/ri"
+import { RiWomenLine, RiVipCrownLine } from "react-icons/ri"
 import { AiOutlineMan } from "react-icons/ai"
+import { IoDiamondOutline } from "react-icons/io5"
 
 const Categories = () => {
+
+  const [showwomen, setShowwomen] = useState<Boolean>(true)
+  const [showmen, setshowmen] = useState<Boolean>(true)
+  const [showkids, setshowkids] = useState<Boolean>(true)
+  const [showsports, setShowsports] = useState<Boolean>(true)
+  const [showbeauty, setShowbeauty] = useState<Boolean>(true)
+  const [showjewelry, setShowjewelry] = useState<Boolean>(true)
+
+  const togglewomen = () =>{
+    setShowwomen(true)
+    setshowmen(false)
+    setshowkids(false)
+    setShowsports(false)
+    setShowbeauty(false)
+    setShowjewelry(false)
+  }
+  
+  const togglemen = () =>{
+    setShowwomen(false)
+    setshowmen(true)
+    setshowkids(false)
+    setShowsports(false)
+    setShowbeauty(false)
+    setShowjewelry(false)
+  }
+  
+  const togglekids = () =>{
+    setShowwomen(false)
+    setshowmen(false)
+    setshowkids(true)
+    setShowsports(false)
+    setShowbeauty(false)
+    setShowjewelry(false)
+  }
+  
+  const togglesports= () =>{
+    setShowwomen(false)
+    setshowmen(false)
+    setshowkids(false)
+    setShowsports(true)
+    setShowbeauty(false)
+    setShowjewelry(false)
+  }
+  
+  const togglebeauty = () =>{
+    setShowwomen(false)
+    setshowmen(false)
+    setshowkids(false)
+    setShowsports(false)
+    setShowbeauty(true)
+    setShowjewelry(false)
+  }
+  
+  const togglejewelry = () =>{
+    setShowwomen(false)
+    setshowmen(false)
+    setshowkids(false)
+    setShowsports(false)
+    setShowbeauty(false)
+    setShowjewelry(true)
+  }
+  
+  
   return (
     <div>
       <Container>
         <Wrapper>
           <h1>Start exploring.</h1>
           <Category>
-            <Div>
+            {
+              showwomen ? <Div onClick={togglewomen} bcc = "true" cc = "true">
               <span><RiWomenLine /></span>
               Women
-            </Div>
-            <Div>
+            </Div> : ""
+            }
+            {
+              showmen ? <Div onClick={togglemen} bcc = "true" cc = "true">
               <span><AiOutlineMan /></span>
               Man
-            </Div>
-            <Div>
+            </Div> : null
+            }
+            {/* <Div>
               <span><RiWomenLine /></span>
               Kids
             </Div>
@@ -27,13 +95,13 @@ const Categories = () => {
               Sports
             </Div>
             <Div>
-              <span><RiWomenLine /></span>
+              <span><RiVipCrownLine /></span>
               Beauty
             </Div>
             <Div>
-              <span><RiWomenLine /></span>
+              <span><IoDiamondOutline /></span>
               Jewelry
-            </Div>
+            </Div> */}
           </Category>
         </Wrapper>
       </Container>
@@ -43,13 +111,11 @@ const Categories = () => {
 
 export default Categories;
 
-const Div = styled.div`
+const Div = styled.div<{bcc: string; cc: string}>`
   width: 150px;
   /* height: 50px; */
   border-radius: 30px;
   padding: 10px 15px 10px 15px;
-  background-color: black;
-  color: #fff;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -59,11 +125,13 @@ const Div = styled.div`
     margin-right: 10px;
     font-size: 25px;
   }
+  background-color: ${({bcc}) => bcc ? "#0F172A": "#DEEBFF"};
+  color: ${({cc}) => cc ? "#fff" : "#0F172A"};
 `;
 const Category = styled.div`
   width: 800px;
   height: 50px;
-  padding: 10px 0px 10px 0px;
+  padding: 5px 0px 5px 0px;
   background-color: #fff;
   border-radius: 40px;
   color: #0F172A;
