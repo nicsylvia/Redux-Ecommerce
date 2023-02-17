0b526aea9c2b085e892fe0e93a88ffcd13cb8c80
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 08:15:50 -0800

    working on cart page

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import MainCartPage from './MainCartPage'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>
+      <MainCartPage />
+    </div>
   )
 }
 
---
+++ src/Components/Cart/CartProps.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+// import CardProductProps from "../Props/CartProductProps";
+import { MdOutlineDoNotDisturb } from "react-icons/md";
+import pic from "../images/hero.png";
+import { AiOutlineCheck } from "react-icons/ai";
+// import SummaryProps from "../Props/SummaryProps";
+
+
+const CartProps = () => {
+  return (
+    <div>CartProps</div>
+  )
+}
+
+export default CartProps
