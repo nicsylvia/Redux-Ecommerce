e3c3daa58e99752c270fe9e2419247456a4d52c6
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 08:17:14 -0800

    still on cart props

--- src/Components/Cart/CartProps.tsx
+++ src/Components/Cart/CartProps.tsx
@@ -13,4 +13,45 @@ const CartProps = () => {
   )
 }
 
-export default CartProps
+export default CartProps;
+
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  width: calc(100vw - 200px);
+`;
+const Head = styled.div`
+  height: 230px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-bottom: 1px solid #d1cece;
+`;
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 38px;
+`;
+const Split = styled.div`
+  margin-top: 20px;
+  font-weight: 600;
+  font-size: 14px;
+  color: #686666;
+  span {
+    text-decoration: underline;
+  }
+`;
+const Body = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Product = styled.div`
+  margin-right: 50px;
+`;
+const Summary = styled.div`
+  flex: 1;
+  position: relative;
+`;
---
+++ src/Components/Cart/MainCartPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainCartPage = () => {
+  return (
+    <div>MainCartPage</div>
+  )
+}
+
+export default MainCartPage
