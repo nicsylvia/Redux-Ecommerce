4a161e9e2f00bbcda556dc0ab29e4ee48c8aa1cf
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 08:28:49 -0800

    working on main cart page

--- src/Components/Cart/MainCartPage.tsx
+++ src/Components/Cart/MainCartPage.tsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { VscPaintcan } from "react-icons/vsc";
+import { BsArrowsAngleExpand } from "react-icons/bs";
+
+interface iprops {
+  price: number;
+  dp: any;
+  name: string;
+  status: string;
+  sign: any;
+}
 
 const MainCartPage = () => {
   return (
