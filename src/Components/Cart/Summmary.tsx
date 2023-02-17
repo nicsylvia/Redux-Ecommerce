86256799053e26d9a192df939a4c99ae5ee2f02c
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 08:25:32 -0800

    working on cart summary

--- src/Components/Cart/Summmary.tsx
+++ src/Components/Cart/Summmary.tsx
@@ -4,7 +4,43 @@ import { MdOutlineDoNotDisturb } from "react-icons/md";
 
 const Summmary = () => {
   return (
-    <div>Summmary</div>
+    <div>
+        <Container>
+        <Wrapper>
+          <Title>Order Summary</Title>
+          <Summaries>
+            <Records>
+              <TextDiv>Subtotal</TextDiv>
+              <NumberDiv>$3663</NumberDiv>
+            </Records>
+            <Records>
+              <TextDiv>Shipping extimate</TextDiv>
+              <NumberDiv>$3663</NumberDiv>
+            </Records>
+            <Records>
+              <TextDiv>Tax estimate</TextDiv>
+              <NumberDiv>$3663</NumberDiv>
+            </Records>
+            <Records2>
+              <TextDiv2>Order Total</TextDiv2>
+              <NumberDiv2>$3663</NumberDiv2>
+            </Records2>
+          </Summaries>
+          <Button>
+            <button>Checkout</button>
+          </Button>
+          <Ending>
+            <div>
+              <MdOutlineDoNotDisturb />
+            </div>
+            <div>
+              Learn more <span>Taxes</span> and <span>Shipping</span>
+              Information
+            </div>
+          </Ending>
+        </Wrapper>
+      </Container>
+    </div>
   )
 }
 
