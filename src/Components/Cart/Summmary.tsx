71291db31f4996c291ced5071aa38d38c441ab8f
nicsylvia <nicsylvia15f@gmail.com>
2023-02-17 08:20:21 -0800

    saved summary props

---
+++ src/Components/Cart/Summmary.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { MdOutlineDoNotDisturb } from "react-icons/md";
+
+const Summmary = () => {
+  return (
+    <div>Summmary</div>
+  )
+}
+
+export default Summmary
