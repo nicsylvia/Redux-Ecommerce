5e1ce4fc5887bfaab4aa323cf06ca0e95fa0c43a
nicsylvia <nicsylvia15f@gmail.com>
2023-02-19 00:25:35 -0800

    working on single products

---
+++ src/Components/DetailScreen/SinglePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { AiFillStar } from "react-icons/ai";
+
+const SinglePage = () => {
+	return (
+		<Container>
+			<First></First>
+			<Second>
+				<h2>Heavy Weight Shoes</h2>
+				<PriceHold>
+					<Price>$30.00</Price>
