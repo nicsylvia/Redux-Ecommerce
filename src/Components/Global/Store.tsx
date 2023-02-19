40af3fa4718d6d4420b9d68e188aac47154d5950
nicsylvia <nicsylvia15f@gmail.com>
2023-02-19 00:20:39 -0800

    saved store

---
+++ src/Components/Global/Store.tsx
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux/es/exports";
+import myReducer from "./ReduxState";
+
+export const store = configureStore({
+	reducer: {
+		myReducer,
+	},
+});
+
+// define your despatch and selector
+
+export const UseAppDispach: () => typeof store.dispatch = useDispatch;
+
+// export const UseAppSelector : ReturnType<typeof store.getState> = useSelector
