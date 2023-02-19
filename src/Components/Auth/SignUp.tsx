039b4d272f6734e1aac52d7a98abbb957b882ef3
nicsylvia <nicsylvia15f@gmail.com>
2023-02-18 23:51:55 -0800

    working on register

--- src/Components/Auth/SignUp.tsx
+++ src/Components/Auth/SignUp.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { CreateUser } from "../Api/Api";
+import { UseAppDispach } from "../Global/Store";
+import { loginUser } from "../Global/ReduxState";
+
 const SignUp = () => {
   return (
     <div>SignUp</div>
