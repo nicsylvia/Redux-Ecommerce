7293ac41c290ff4c223ba94cf84818665e66cc33
nicsylvia <nicsylvia15f@gmail.com>
2023-02-20 10:42:26 -0800

    saved store

--- src/Components/Global/Store.tsx
+++ src/Components/Global/Store.tsx
@@ -1,16 +1,16 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { useDispatch } from "react-redux";
-// import { useSelector } from "react-redux/es/exports";
-// import myReducer from "./ReduxState";
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useSelector } from "react-redux/es/exports";
+import myReducer from "./ReduxState";
 
-// export const store = configureStore({
-// 	reducer: {
-// 		myReducer,
-// 	},
-// });
+export const store = configureStore({
+	reducer: {
+		myReducer,
+	},
+});
 
-// // define your despatch and selector
+// define your despatch and selector
 
-// export const UseAppDispach: () => typeof store.dispatch = useDispatch;
+export const UseAppDispach: () => typeof store.dispatch = useDispatch;
 
-// // export const UseAppSelector : ReturnType<typeof store.getState> = useSelector
+export const UseAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector
