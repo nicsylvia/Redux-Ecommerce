6727b57eab747688c579dcb20835df100d0ad771
nicsylvia <nicsylvia15f@gmail.com>
2023-02-22 00:52:46 -0800

    saved create user API

--- src/Components/API/Api.tsx
+++ src/Components/API/Api.tsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+interface UserData {
+    name: string,
+    email: string,
+    password: string,
+}
+
 const Endpoint = "http://localhost:4573/api"
 
-export const CreateNewUser = async() =>{
-    return await axios.post(`${Endpoint}/register`)
+export const CreateNewUser = async({name, email, password}: UserData) =>{
+    return await axios.post(`${Endpoint}/register`,{name, email, password})
 }
