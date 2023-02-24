a47ac9b076c29b306d90a9c9597b5bf0cdef4474
nicsylvia <nicsylvia15f@gmail.com>
2023-02-23 16:24:47 -0800

    working on redux persist

--- src/Components/Global/Store.tsx
+++ src/Components/Global/Store.tsx
@@ -1,40 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
 import myReducer from "./ReduxState";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import { configureStore } from '@reduxjs/toolkit'
 import {
-	persistStore,
-	persistReducer,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-} from "redux-persist";
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const persistConfig = {
-	key: 'ecommerces',
+	key: 'Ecommerce',
 	version: 1,
-	Storage
-  };
+	storage,
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, myReducer)
 
-  const userPersistedReducer = persistReducer(persistConfig, myReducer)
+//   export const Store = configureStore({
+// 		reducer: userPersistedReducer,
+// 		middleware: (getDefaultMiddleware) =>
+// 			getDefaultMiddleware({
+// 				serializableCheck: {
+// 					ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+// 				},
+// 			}),
+// 	});
 
-  export const Store = configureStore({
-		reducer: userPersistedReducer,
-		middleware: (getDefaultMiddleware) =>
-			getDefaultMiddleware({
-				serializableCheck: {
-					ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-				},
-			}),
-	});
-
-// export const Store = configureStore({
-// 	reducer: {
-// 		myReducer
-// 	}
-// });
+export const Store = configureStore({
+	reducer: {
+		myReducer
+	}
+});
 
 // Define your dispatch and your selector
 
