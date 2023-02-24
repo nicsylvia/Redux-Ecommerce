c7a3e7edaaaa5f4195561fca10e9cc7e6d5a3142
nicsylvia <nicsylvia15f@gmail.com>
2023-02-24 15:16:27 -0800

    working on products page

--- src/Components/DetailScreen/AllProducts.tsx
+++ src/Components/DetailScreen/AllProducts.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const AllProducts = () => {
   return (
@@ -46,4 +48,158 @@ const AllProducts = () => {
   )
 }
 
-export default AllProducts
+export default AllProducts;
+
+const Five = styled.div`
+height: 5vh;
+width: 200px;
+display: flex;
+justify-content: center;
+align-items: center;
+
+span{
+    font-size: 20px;
+    margin-top: 5px;
+    margin-right: 5px;
+ 
+}
+p{
+    color: #00000065;
+    font-weight: 600;
+}`
+
+const Four = styled.div`
+height: 5vh;
+width: 80px;
+border: 3px solid #22C55E;
+border-radius:10px;
+display: flex;
+align-items: center;
+justify-content: center;
+font-weight: 900;
+color: #22C55E;`
+
+const Three = styled.div`
+height: 10vh;
+display: flex;
+justify-content: space-between;
+align-items: center;`
+
+const Two = styled.div`
+h3{
+    margin: 0;
+    font-weight: 500;
+    color: #000000;
+    font-size: 16px;
+    margin-top: 10px;
+
+}`
+const One = styled.div`
+h2{
+    margin: 0;
+    color: #000;
+    font-size:20px;
+    font-weight: 600;
+}`
+const ProductImg = styled.div`
+    width: 100%;
+    height: calc(100% - 40px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        margin-bottom: 100px;
+    }
+`
+
+const Heart = styled.div`
+width: 40px;
+height: 40px;
+    background-color: #fff;
+    margin-right: 20px;
+    border-radius: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    span{
+        font-size: 20px;
+        color: #000;
+        margin-top: 5px;
+        
+    }
+
+;
+`
+const Wishlist = styled.div`
+    width: 100%;
+    height: 8vh;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+`
+const Second =styled.div`
+width: 100%;
+height: 35%;
+display: flex;
+justify-content: flex-end;
+flex-direction: column`
+
+const First =styled.div`
+width: 320px;
+height: 65%;
+background-color: #eff4fa;
+border-radius: 30px;
+overflow: hidden;
+
+;
+`
+
+const Display = styled(NavLink)`
+    width: 320px;
+    height: 100%;
+    text-decoration: none;
+
+`
+const Products = styled.div`
+    width: 100%;
+    height: 65vh;
+    /* background-color: #00ff62; */
+    display: flex;
+ 
+`
+
+const Container = styled.div`
+    width: 100%;
+    padding: 20px 0px 20px 0px;
+    /* background-color: red; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 90%;
+    padding: 10px 0px 10px 0px;
+    display: flex;
+    flex-direction: column;
+    span{
+        color: gray;
+        font-weight: 600;
+    }
+
+    h1{
+              font-weight: 600;
+    }
+
+    hr{
+    
+        width: 100%;
+        border: 1px solid #e2dfdf;
+        margin-top: 40px;
+    }
+`;
