dd170b7a3ef61ab0d7fabd358ebcfd2ff42275ea
nicsylvia <nicsylvia15f@gmail.com>
2023-02-25 15:49:34 -0800

    saved all

--- src/Components/Global/ReduxState.tsx
+++ src/Components/Global/ReduxState.tsx
@@ -45,6 +45,13 @@ const ReduxState = createSlice({
 			})
 		}
 		state.totalQuantity += 1
+		state.totalPrice
+			state.totalPrice +=
+			state.cart[check].cartQuantity * state.cart[check].price;
+			state.totalPrice = state.cart.reduce(
+			(accc, next) => accc + next.cartQuantity * next.price,
+			0,
+			);
 	},
 	removeFromCart: (state, {payload}: PayloadAction<CartData>) =>{
 		const outOfCart = state.cart.findIndex((position) =>
